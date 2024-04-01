8cf1478577d3e2c130a8120561d575404e3e6bb8
Ryan <rlemus93@gmail.com>
2024-04-01 11:33:45 -0700

    adds cat resources and spec file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+cats = [
+  {
+    name: 'Leif',
+    age: 2,
+    enjoys: 'loves the ice machine on the fridge.',
+    image: 'https://media.istockphoto.com/id/1370365587/photo/big-eyed-naughty-obese-cat-looking-at-the-target.webp?b=1&s=170667a&w=0&k=20&c=468okwKwMosVm_DxsNTfe5ms9h5_8w0ETSk51Ys6I5k='
+  },
+
+  {
+    name: 'Kopi',
+    age: 13,
+    enjoys: 'likes to sleep and scratch people',
+    image: 'https://media.istockphoto.com/id/611312482/photo/portrait-of-attentive-resting-on-the-sofa-white-grey-cat.webp?b=1&s=170667a&w=0&k=20&c=RUcLIzZYHLI--F2npa9Gi8HcAtScXaSjGTFPzDbbUg0='
+  },
+
+  {
+    name: 'Cali',
+    age: 1,
+    enjoys: 'loved going on long walks to the store',
+    image: 'https://media.istockphoto.com/id/1428651837/photo/staring-cat.jpg?s=1024x1024&w=is&k=20&c=cJhplE9ye-SqudOMJZ3dLMqGqmKZKKeNNEP-RwDEw2E='
+  }
+]
+
+cats.each do |cat|
+  Cat.create(cat)
+  puts "Creating cat: #{cat}"
+end
